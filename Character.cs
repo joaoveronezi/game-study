32eebe12254f7e5de1c857ef61c417ebdf488e18
joaoveronezi <veronezijoao01@gmail.com>
2023-07-28 15:43:54 -0300

    Testing some stuff, adding builded content elements

---
+++ Character.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace firstProject
+{
+    public class Character
+    {
+        private Texture2D texture;
+        private Vector2 position;
+        private Vector2 velocity;
+        private bool hasJumped;
+
+
+        // Constructor
+        public Character(Vector2 position, Texture2D texture)
+        {
+
+            this.position = position;
+            this.texture = texture;
+            velocity = new Vector2(0, 0);
+            hasJumped = true;
+
+
+        }
+        public void Update(GameTime gameTime)
+        {
+            position += velocity;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) velocity.X = 3f;
+            else  if (Keyboard.GetState().IsKeyDown(Keys.Left)) velocity.X -= 3f; else velocity.X = 0f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && hasJumped == false)
+            {
+                position.Y -= -10f;
+                velocity.Y = -5f;
+                hasJumped = true;
+            }
+
+            if (hasJumped == true)
+            {
+                float i = 1;
+                velocity.Y += 0.15f * i;
+            } 
+
+
+            if(position.Y + texture.Height > 350)
+            {
+                hasJumped = false;
+            }
+
+            if (hasJumped == false)
+            {
+                velocity.Y += 0;
+            }
+
+        }
+
+        // Draw method, basically put it on screen.
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+
+        }
+    }
+
+
+
+
+}
--- Game1.cs
+++ Game1.cs
@@ -9,11 +9,25 @@ namespace firstProject
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+
+        private Texture2D charSprite;
+        private Texture2D backgroundSprite;
+        private Texture2D objectSprite;
+
+        private Vector2 playerPosition;
+
+        const int charHitbox = 45;
+
+        Character player;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+
+
         }
 
         protected override void Initialize()
@@ -27,7 +41,18 @@ namespace firstProject
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            charSprite = Content.Load<Texture2D>("ladie");
+            backgroundSprite = Content.Load<Texture2D>("sky");
+            objectSprite = Content.Load<Texture2D>("tree");
+
+
+            // Set the resolution to match the background image dimensions
+            //_graphics.PreferredBackBufferWidth = backgroundSprite.Width;
+            //_graphics.PreferredBackBufferHeight = backgroundSprite.Height;
+            //_graphics.ApplyChanges();
+
             // TODO: use this.Content to load your game content here
+            player = new Character(new Vector2(50, 50), charSprite);
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +62,10 @@ namespace firstProject
 
             // TODO: Add your update logic here
 
+      
+            
+            player.Update(gameTime);
+
             base.Update(gameTime);
         }
 
@@ -44,7 +73,23 @@ namespace firstProject
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+
+            // Calculate the position to center the background image
+            int screenWidth = GraphicsDevice.Viewport.Width;
+            int screenHeight = GraphicsDevice.Viewport.Height;
+            int backgroundX = (screenWidth - backgroundSprite.Width) / 2;
+            int backgroundY = (screenHeight - backgroundSprite.Height) / 2;
+
+
+
             // TODO: Add your drawing code here
+            // start the draw
+            _spriteBatch.Begin();
+           // _spriteBatch.Draw(backgroundSprite, new Vector2(backgroundX, backgroundY), Color.White);
+            //_spriteBatch.Draw(objectSprite, new Vector2(0,0), Color.White);
+            //_spriteBatch.Draw(charSprite, playerPosition, Color.White);
+            player.Draw(_spriteBatch);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
